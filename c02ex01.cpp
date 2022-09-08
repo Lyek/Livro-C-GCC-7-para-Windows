048671e41cc7a6c819c8e9f9a73d356f2ef7a214
Ulisses Shiramoto <39807162+Lyek@users.noreply.github.com>
2022-09-07 21:26:35 -0300

    Add files via upload

---
+++ c02ex01.cpp
@@ -0,0 +1,15 @@
+// C02EX01.CPP
+
+#include <iostream>
+using namespace std;
+
+int main (void)
+{
+
+  cout << "Ola, Mundo!" ;
+  cout << endl;
+
+  cout <<"Tecle <Enter> para encerrar..." << endl;
+  cin.get();
+  return 0;
+}
